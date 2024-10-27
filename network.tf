30cf84d1930fe6d67c40b54d8b9c1257bf1fea63
EC2 Default User <ec2-user@ip-172-31-30-247.ec2.internal>
2024-10-27 21:27:34 +0000

    chore: create variables file and refactor variables

--- network.tf
+++ network.tf
@@ -1,8 +1,3 @@
-variable "vpc_name" {
-    type = string
-    default = "vpc_clc12_terraform"
-}
-
 resource "aws_vpc" "minha_vpc" {
   cidr_block       = "10.0.0.0/16"
 
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_name" {
+    type = string
+    default = "vpc_clc12_terraform"
+}
